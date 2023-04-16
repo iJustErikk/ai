3e2732819846b719994239f3ba4cb73397082b66
Erik Awwad <awadoferik@gmail.com>
2023-04-16 17:00:59 -0400

    fix image upload

--- vector-db/frontend/src/App.jsx
+++ vector-db/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FileUploader } from "react-drag-drop-files";
 import './App.css';
 import defaultImage from "./assets/default.jpeg";
@@ -7,6 +7,17 @@ const port = 8000;
 function App() {
   const [selectedImage, setSelectedImage] = useState(defaultImage);
   const [searchResults, setSearchResults] = useState([]);
+  const [selectedImageSrc, setSelectedImageSrc] = useState(defaultImage);
+
+  useEffect(() => {
+    if (selectedImage == defaultImage) return;
+    const reader = new FileReader();
+    reader.onload = () => {
+        const srcData = reader.result;
+        setSelectedImageSrc(srcData);
+    };
+    reader.readAsDataURL(selectedImage);
+  }, [selectedImage]);
 
   const handleUpload = async (event) => {
     event.preventDefault();
@@ -81,7 +92,7 @@ function App() {
   return (
     <div className="App">
       <h1>Reverse Image Search</h1>        
-      <img className='selected-image' src={selectedImage} alt="selected image"/>
+      <img className='selected-image' src={selectedImageSrc} alt="selected image"/>
       <p>Selected Image</p>
       <FileUploader handleChange={setSelectedImage} name="file" types={['png', 'jpg', 'jpeg']} />
       <div style = {{ marginTop : 20 }}>
